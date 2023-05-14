ab91976a965b37ea806ea72ae32d72dfbb7760f1
Farshid Ganjali <f.ganjali@tech1a.co>
2023-05-14 17:13:21 +0330

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
       <Head>
             <meta name="color-scheme" content="light only"/>
             <link rel='shortcut icon' href='/img.png' />
+            <script src="/static/assets/js/env-config.js"></script>
       </Head>
       <body className={"h-screen bg-gray-100 text-black flex flex-col bg-[url(/icons/logo-raw.svg)] bg-no-repeat bg-center bg-contain"}>
         <Main />
