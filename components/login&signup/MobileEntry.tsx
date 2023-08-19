416745caebaf8bc1758bc0dc4ee3b1ce3d1fc0b2
S.Anvari <S@123456$>
2023-08-19 15:43:53 +0330

    refcode

--- components/login&signup/MobileEntry.tsx
+++ components/login&signup/MobileEntry.tsx
@@ -1,19 +1,19 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CaptchaComponent from "../common/component/CaptchaComponent";
-import {Form, Formik} from "formik";
-import {toast} from "react-toastify";
+import { Form, Formik } from "formik";
+import { toast } from "react-toastify";
 import InputComponent from "../common/component/InputComponent";
-import {IdpContext} from "../../pages/[[...code]]";
-import {mobileEntry} from "../common/shcema/schema";
+import { IdpContext } from "../../pages/[[...code]]";
+import { mobileEntry } from "../common/shcema/schema";
 import useQuery from "../../hooks/useQuery";
-import {IDP_URL} from "../../api/constants";
-import {useAuth} from "react-oidc-context";
-import {useRouter} from "next/router";
+import { IDP_URL } from "../../api/constants";
+import { useAuth } from "react-oidc-context";
+import { useRouter } from "next/router";
 
 
 export default function MobileEntry() {
-    const {setLevel, setMobile, mobile} = useContext<any>(IdpContext)
-    const {fetchAsyncData} = useQuery({url: `${IDP_URL}/api/account/notifications/register/sms`})
+    const { setLevel, setMobile, mobile } = useContext<any>(IdpContext)
+    const { fetchAsyncData } = useQuery({ url: `${IDP_URL}/api/account/notifications/register/sms` })
     type initialType = {
         mobile: string,
         password: string,
@@ -34,12 +34,12 @@ export default function MobileEntry() {
         let _info: any = {};
         _info[key] = value;
 
-        setInfo({...info, ..._info})
+        setInfo({ ...info, ..._info })
     }
 
 
     const submitHandler = async (v: any) => {
-        await fetchAsyncData({phoneNumber: v.mobile, captchaToken: info.uuid + '_' + v.captcha})
+        await fetchAsyncData({ phoneNumber: v.mobile, captchaToken: info.uuid + '_' + v.captcha })
             .then(() => {
                 setMobile(v.mobile)
                 setLevel('confirmation')
@@ -53,7 +53,7 @@ export default function MobileEntry() {
     const router = useRouter()
 
     useEffect(() => {
-        localStorage.setItem('RefCode', `${router.asPath !== '/' && router.asPath !== '/[[...code]]' ? (router.asPath).split('/')[1] : ''}`)
+        localStorage.setItem('RefCode', `${router.asPath !== '/' && router.asPath !== '/[[...code]]' ? (router.asPath).split('/').at(-1) : ''}`)
     }, [router.asPath])
 
     return (
@@ -61,17 +61,17 @@ export default function MobileEntry() {
             <h2>به توانا خوش آمدید</h2>
             <p>جهت ثبت نام شماره موبایل خود را وارد کنید.</p>
             <Formik initialValues={initialValue} validationSchema={mobileEntry} validateOnChange={false}
-                    onSubmit={submitHandler}>
-                {({isSubmitting}) => (
+                onSubmit={submitHandler}>
+                {({ isSubmitting }) => (
                     <Form className={'justify-end space-y-10 grow flex flex-col'}>
                         <InputComponent label={'شماره موبایل'}
-                                        name={'mobile'}
-                                        type={'text'}/>
+                            name={'mobile'}
+                            type={'text'} />
                         <span className={'mt-4'}>
                             <CaptchaComponent infoUpdate={infoUpdate}
-                                              info={info}
-                                              name={'captcha'}
-                                              retry={retry}
+                                info={info}
+                                name={'captcha'}
+                                retry={retry}
                             />
                         </span>
                         <div className={'text-center'}>
@@ -80,9 +80,9 @@ export default function MobileEntry() {
                                     ثبت نام
                                     {isSubmitting && <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
                                         <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
-                                                strokeWidth="4"></circle>
+                                            strokeWidth="4"></circle>
                                         <path className="opacity-75" fill="currentColor"
-                                              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                                     </svg>}
                                 </div>
                             </button>
