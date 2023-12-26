fdbeee26b7bcefc16a4895716a8f0264fcb42bc5
madsadi <sadixus@gmail.com>
2023-12-26 15:26:18 +0330

    seal and signature

--- components/main/agreement/page-header-footer.tsx
+++ components/main/agreement/page-header-footer.tsx
@@ -1,36 +1,46 @@
 import moment from "jalali-moment";
-import Image from 'next/image'
+import Image from "next/image";
 import { memo, useContext } from "react";
 import { agreementContext } from "./agreement-level";
 
 const PageHeaderFooter = () => {
-    const { document } = useContext<any>(agreementContext)
-    return (
-        <>
-            <div className="page-header">
-                <div className="logo-card relative">
-                    <img src={"/logo-black.svg"} className={'h-20 w-20'} alt="tavana" />
-                </div>
-                <div className="mt-5 flex font-weight-bold">
-                    <div className={'text-sm ml-2'}> تاریخ :</div>
-                    <span>
-                        {moment().locale('fa').format('YYYY/MM/DD')}
-                    </span>
-                </div>
-            </div>
-            <div className="page-footer">
-                <div className="text-right">
-                    <div className={'titleValue'}> امضاء مشتري / نماینده :</div>
-                    <div className="sign-card relative">
-                        <Image src={`${document?.[0]?.image ? document?.[0]?.image : ''}` || 'null'} fill alt="signPhoto" quality={0} unoptimized={true} />
-                    </div>
-                </div>
-                <div className="text-left">
-                    <div className={'titleValue'}> امضاء مسئول پذیرش :</div>
-                </div>
-            </div>
-        </>
-    )
-}
+  const { document } = useContext<any>(agreementContext);
+  return (
+    <>
+      <div className="page-header">
+        <div className="logo-card relative">
+          <img src={"/logo-black.svg"} className={"h-20 w-20"} alt="tavana" />
+        </div>
+        <div className="mt-5 flex font-weight-bold">
+          <div className={"text-sm ml-2"}> تاریخ :</div>
+          <span>{moment().locale("fa").format("YYYY/MM/DD")}</span>
+        </div>
+      </div>
+      <div className="page-footer">
+        <div className="text-right">
+          <div className={"titleValue"}> امضاء مشتري / نماینده :</div>
+          <div className="sign-card relative">
+            <Image
+              src={
+                `${document?.[0]?.image ? document?.[0]?.image : ""}` || "null"
+              }
+              fill
+              alt="signPhoto"
+              quality={0}
+              unoptimized={true}
+            />
+          </div>
+        </div>
+        <div className="text-left">
+          <div className={"titleValue"}>امضاء مسئول پذیرش :</div>
+          <div className="sign-card relative">
+            <Image src={"/taheri-signature.png"} fill alt="seal" />
+            <Image src={"/tavana-seal.png"} fill alt="seal" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default memo(PageHeaderFooter);
--- public/static/assets/js/env-config.js
+++ public/static/assets/js/env-config.js
@@ -1,99 +1,84 @@
 window._env_ = 
 {
-    //------------------- General Keys ----------------------------
-    "OnlineTradingGatewayEndPoint": "http://stgotg.tech1a.co",
-    "IdpEndPoint": "https://stgidp.tech1a.co",
-    "JarchiEndPoint": "http://jarchi.stg-oms.svc.cluster.local:80",
-    "CommissionStoreEndPoint": "http://commission-store.tech1a.co",
-    "CommissionStoreRPCEndPoint": "http://stgcommission-storeRpc.tech1a.co",
-    "NetFlowEndPoint": "http://stgnetflow.tech1a.co",
-    "NetFlowRPCEndPoint": "http://stgnetflowrpc.tech1a.co",
-    "AdminGatewayEndPoint": "http://stgadmin-gateway.tech1a.co",
-    "AdminPanelEndPoint": "http://stgadmin-panel-client.tech1a.co",
-    "OnlineTradingWebEndPoint": "http://stgotwebclient.tech1a.co",
-    "CustomerManagementRPCEndPoint": "http://customer-management.stg-oms.svc.cluster.local:80",
+    //------------------- EndPoints ----------------------------
+    "OnlineTradingGatewayEndPoint": "http://cluster.tech1a.co",
+    "IdpEndPoint": "https://cluster.tech1a.co",
+    "JarchiEndPoint": "http://cluster.tech1a.co:8090",
+    "CommissionStoreEndPoint": "http://cluster.tech1a.co:9007",
+    "CommissionStoreRPCEndPoint": "http://cluster.tech1a.co:9076",
+    "NetFlowEndPoint": "http://cluster.tech1a.co:8091",
+    "NetFlowRPCEndPoint": "http://cluster.tech1a.co:8099",
+    "AdminGatewayEndPoint": "http://cluster.tech1a.co:9020",
+    "AdminPanelEndPoint": "http://cluster.tech1a.co:9021",
+    "OnlineTradingWebEndPoint": "http://cluster.tech1a.co:9026",
+    "CustomerManagementRPCEndPoint": "http://cluster.tech1a.co:9035",
+    "CustomerManagementRPCEndPointP": "http://cluster.tech1a.co:9035",
     "MobileTradingEndPoint": "http://cluster.tech1a.co:9038",
-    "SejamGatewayEndPoint": "http://stgsejam-gateway.tech1a.co",
-    "FileManagerEndPoint": "http://stgfile-manager.tech1a.co",
-    "SejamReactEndPoint": "http://stgsejam.tech1a.co",
-    "WalletEndPoint": "http://atgwallet.tech1a.co",
-    "MarketDataProviderEndPoint": "http://stgmarket-data-provider.tech1a.co",
-    "MarketDataProviderRPCEndPoint": "http://stgmarket-data-provider.stg-oms.svc.cluster.local:80",
-	"OrderMediatorRPCEndPoint": "http://stgorder-mediatorRPC.tech1a.co",
-	"OrderStoreRPCEndPoint": "http://stgorder-storeRPC.tech1a.co",
-	"WatchlistRPCEndPoint": "http://stgwatchlistRPC.tech1a.co",
-	"PortfolioRPCEndPoint": "http://stgportfolioRPC.tech1a.co",
-	"BookBuildingRPCEndPoint": "http://stgBook-Building-store.tech1a.co",
-	"OrderSwitchRPCEndPoint": "http://stgorder-switch.stg-oms.svc.cluster.local:80",
-	"MarketRuleRPCEndPoint": "http://stgmarket-rule-store.tech1a.co",
-	"IdpUIEndPoint": "https://stgidp-client.tech1a.co",
-	"TbsEndPoint": "http://tbsapi.onlintech1a.co/api",
-    "OnlineRegistrationEndPoint": "http://OnlineRegistrationEndPoint.tech1a.co",
-
-    "MarketerEndPoint": "http://stgmarketer.tech1a.co",
-    "MarketerApiEndPoint": "http://stgmarketer-api.tech1a.co",
-    "MarketDataProviderRPCEndPoint": "http://MarketDataProviderRPC.tech1a.co:",
-    "CaptchaEndPoint": "https://stgcaptcha.tech1a.co",
-
+    "SejamGatewayEndPoint": "http://cluster.tech1a.co:9072",
+    "FileManagerEndPoint": "http://cluster.tech1a.co:9073",
+    "SejamReactEndPoint": "http://cluster.tech1a.co:9075",
+    "WalletEndPoint": "http://cluster.tech1a.co:9077",
+    "MarketerEndPoint": "http://devmarketer.tech1a.co",
+    "MarketerApiEndPoint": "http://devmarketer-api.tech1a.co",
+    "MarketDataProviderEndPoint": "https://dev-mdp.tech1a.co",
+    "MarketDataProviderRPCEndPoint": "http://cluster.tech1a.co:9019",
+	"OrderMediatorRPCEndPoint": "http://cluster.tech1a.co:9027",
+	"OrderStoreRPCEndPoint": "http://cluster.tech1a.co:9028",
+	"WatchlistRPCEndPoint": "http://cluster.tech1a.co:9025",
+	"PortfolioRPCEndPoint": "http://cluster.tech1a.co:9033",
+	"BookBuildingRPCEndPoint": "http://cluster.tech1a.co:9023",
+	"OrderSwitchRPCEndPoint": "http://cluster.tech1a.co:9039",
+	"MarketRuleRPCEndPoint": "http://cluster.tech1a.co:9022",
+	"IdpUIEndPoint": "https://cluster.tech1a.co:8443",
+	"TbsEndPoint": "https://tbsapi.onlinetavana.ir/api",
+    "OnlineRegistrationEndPoint": "http://cluster.tech1a.co:9075",
+    "CreditManagementEndPoint": "http://devcredit-management.tech1a.co",
     "SplunkHostEndPoint": "log.tech1a.co",
-    "SplunkPort": 5142,
-    "KafkaBootstrapServersEndPoint": "kafka-stg-kafka-headless.stg-kafka.svc.cluster.local:9092",
-    "RegisterServicePermissionEndPoint": "https://stgidp.tech1a.co/api/service-permossion/register-service-permission",
-    "TavanaCaptchaVerificationEndPoint": "https://stgcaptcha.tech1a.co/api/validate",
-    "GoogleRecaptchaVerificationEndPoint": "http://www.google.com/recaptcha/api/siteverify",
+    "SplunkPort": 5141,
+    "KafkaBootstrapServersEndPoint": "172.24.35.22:29092",
+    "RegisterServicePermissionEndPoint": "https://cluster.tech1a.co/api/service-permossion/register-service-permission",
+    "TavanaCaptchaVerificationEndPoint": "https://cluster.tech1a.co:8543/api/validate",
+    "GoogleRecaptchaVerificationEndPoint": "https://www.google.com/recaptcha/api/siteverify",
     "GoogleRecaptchaServerKey": "6LeuUk8aAAAAAFjIwrWsSd0yIu5XUySlJ9hyeT38",
+    "CaptchaEndPoint": "https://cluster.tech1a.co:8543",
 
-    //------------------  Jarchi Keys ------------------------------
-    "JarchiConnectionString": "Server=172.24.34.19;Database=Stg_Jarchi;User Id=jarchi;Password=9KAD&*&U)z&wMT(I;",
 
-    //------------------  CustomerManagement Keys ------------------------------
-    "CustomerManagementConnectionString": "Data Source=172.24.34.19;Initial Catalog=Stg_CustomerRegistration;User Id=Customer_User;Password=A%FEg8NppUBQZ45e;MultipleActiveResultSets=True;",
-    "TbsEndPoint": "http://tbsapi.onlintech1a.co/api",
-    "OnlineRegistrationEndPoint": "http://stgsejam.tech1a.co",
+    //--------------------marketer admin--------------------------------------
+	"MARKETER_MONGO_CONNECTION_STRING": "mongodb://root:Passw0rd@172.24.35.21:27017/?directConnection=true",
+    "MARKETER_ADMIN_OPENAPI_URL": "",
+    "MARKETER_ADMIN_ORIGINS": "*",
+    "MARKETER_ADMIN_SPLUNK_INDEX": "dev",
+    "MARKETER_ADMIN_DOCS": "/docs",
+    "MARKETER_ADMIN_REDOC": "/redoc",
+    //--------------------marketer api--------------------------------------
+    "MARKETER_OPENAPI_URL": "/openapi.json",
+    "MARKETER_ORIGINS": "*",
+    "MARKETER_DOCS": "/docs",
+    "MARKETER_REDOC": "/redoc",
+    
 
-    //------------------  Idp Keys ------------------------------
-    "IdpConnectionString": "Server=172.24.34.19;Database=Stg_Idp;User Id=Idp;Password=IDP@97790;",
-    "IdpUIEndPoint": "https://stgidp-client.tech1a.co",
-
-    //------------------  FileManager Keys ------------------------------
-    "FileManagerConnectionString": "Server=172.24.34.19;Database=Stg_FileStream;uid=FileStream;pwd=3WzFgJdQyF4JBhm3;Integrated Security=False",
-        //------------------  Order Mediator Keys ------------------------------
-    "OrderMediatorSQLConnectionString": "Server=172.24.34.19;Database=Stg_OrderStore;ID=oms;Password=Oms@123456;Integrated Security=False",
-    //------------------  Admin Panel Keys ------------------------------
-    "NetflowEndPoint": "http://stgnetflow.tech1a.co",
-    "MarketerAdminEndPoint": "http://stgmarketer-admin.tech1a.co",
-
-    //------------------  CommissionStore Keys ------------------------------
-    "CommissionStoreConnectionString": "Data Source=172.24.34.19;Initial Catalog=Stg_CommissionStore;User ID=comStoreUser;Password=Com@123456;MultipleActiveResultSets=True",	
-    //------------------  BookBuilding Keys ------------------------------
-	"BookBuildingConnectionString": "Data Source=172.24.34.19;Initial Catalog=Stg_BookBuildingStore;User ID=oms;Password=Oms@123456;MultipleActiveResultSets=True",
-    //------------------  CashRequest Keys ------------------------------
-	"CashRequestConnectionString": "Data Source=172.24.34.19;Initial Catalog=Stg_CashRequest;User ID=CashRequestUser;Password=Cash@123456;MultipleActiveResultSets=True",
-    //------------------  IntradayPortfolio Keys ------------------------------
-	"IntradayPortfolioConnectionString": "Data Source=172.24.34.19;Initial Catalog=Stg_PortfolioManagment;User ID=Portfolio;Password=HrxG+egWNdK&Hng(;MultipleActiveResultSets=True;Application Name=My Application;Connection Timeout=120;TrustServerCertificate=True",
-    //------------------  IntradayPortfolioMongo Keys ------------------------------
-	"IntradayPortfolioMongoConnectionString": "mongodb://root:1219S4OiIhSXPJmOGfmdlFlsQ121@mongodb-mongo-mongo-headless.stg-mongodb.svc.cluster.local:27017/",
-    //------------------  IpgTransaction Keys ------------------------------
-	"IpgTransactionConnectionString": "Server=172.24.34.19;Database=Stg_IPGTransaction;uid=ipg;pwd=Ipg@123456;",
-    //------------------  MarketDataProvider Keys ------------------------------
-	"MarketDataProviderConnectionString": "Data Source=172.24.34.19;Initial Catalog=Stg_MarketData;User ID=MarketDataUser;Password=Market@123456;MultipleActiveResultSets=True;Application Name=My Application;Connection Timeout=120;",
-    //------------------  MarketRuleStore Keys ------------------------------
-	"MarketRuleStoreConnectionString": "Data Source=172.24.34.19;Initial Catalog=Stg_MarketRuleStore;User ID=oms;Password=Oms@123456;MultipleActiveResultSets=True",
-    //------------------  NetFlow Keys ------------------------------
-	"NetFlowConnectionString": "server=172.24.34.19;database=Stg_NetFlow;uid=netFlowUser;pwd=NetFlow@123456;",
-    //------------------  OrderMediatorMongo Keys ------------------------------
-	"OrderMediatorMongoConnectionString": "mongodb://root:1219S4OiIhSXPJmOGfmdlFlsQ121@mongodb-mongo-mongo-headless.stg-mongodb.svc.cluster.local:27017/",
-    //------------------  OrderSwitchRedis Keys ------------------------------
-	"OrderSwitchRedisConnectionString": "redis-headless.stg-redis.svc.cluster.local:6379,password=Duk0zfgYJhGWKDre",
-    //------------------  TseTransmitter Keys ------------------------------
-	"TseTransmitterConnectionString": "Data Source=172.24.34.19;Initial Catalog=Stg_TSETransmitter;User ID=transmitterUser;Password=Transmitter@123456;MultipleActiveResultSets=True",
-    //------------------  Wallet Keys ------------------------------
-	"WalletConnectionString": "Server=172.24.34.19;Database=Stg_CashFlow;uid=cfl;pwd=Cfl@123456;",
-    //------------------  Watchlist Keys ------------------------------
-	"WatchlistConnectionString": "Data Source=172.24.34.19;Initial Catalog=Stg_Watchlist;User ID=WatchList_User;Password=Watchlist@1234;MultipleActiveResultSets=True;",
-     //------------------  Credit Management Keys ------------------------------
-    "CreditManagementSQLConnectionString":  "Data Source=172.24.34.19;Initial Catalog=Stg-Credit;User ID=credit-management;Password=123456;MultipleActiveResultSets=True;Application Name=My Application;Connection Timeout=120;TrustServerCertificate=True"
-
+	//------------------- ConnectionStrings ----------------------------
+	"JarchiConnectionString": "Server=172.24.34.11;Database=Jarchi;User Id=jarchi;Password=Jarchi@123456;",
+	"CustomerManagementConnectionString": "Data Source=172.24.34.11;Initial Catalog=CustomerRegistration;User Id=Customer_User;Password=C@123456;MultipleActiveResultSets=True;",
+    "IdpConnectionString": "Server=172.24.34.11;Database=Idp;User Id=Idp;Password=Idp@123456;",
+    "FileManagerConnectionString": "Server=172.24.34.11;Database=FileStream;uid=File_Stream;pwd=FS@123456;Integrated Security=False",
+	"CommissionStoreConnectionString": "Data Source=172.24.34.11;Initial Catalog=Dev_CommissionStore;User ID=comStoreUser;Password=Com@123456;MultipleActiveResultSets=True",	
+	"BookBuildingConnectionString": "Data Source=172.24.34.11;Initial Catalog=dev_BookBuildingStore;User ID=oms;Password=Oms@123456;MultipleActiveResultSets=True",
+	"CashRequestConnectionString": "Data Source=172.30.70.11;Initial Catalog=Dev_CashRequest;User ID=CashRequestUser;Password=Cash@123456;MultipleActiveResultSets=True",
+	"IntradayPortfolioConnectionString": "Data Source=172.24.34.11;Initial Catalog=Dev_PortfolioManagment;User ID=Portfolio;Password=HrxG+egWNdK&Hng(;MultipleActiveResultSets=True;Application Name=My Application;Connection Timeout=120;TrustServerCertificate=True",
+	"IntradayPortfolioMongoConnectionString": "mongodb://root:Passw0rd@172.24.35.21:27017",
+	"IpgTransactionConnectionString": "Server=172.30.70.11;Database=dev_IPGTransaction;uid=ipg;pwd=Ipg@123456;",
+	"MarketDataProviderConnectionString": "Data Source=172.24.34.11;Initial Catalog=Dev_MarketData;User ID=MarketDataUser;Password=Market@123456;MultipleActiveResultSets=True;Application Name=My Application;Connection Timeout=120;",
+	"MarketRuleStoreConnectionString": "Data Source=172.24.34.11;Initial Catalog=dev_MarketRuleStore;User ID=oms;Password=Oms@123456;MultipleActiveResultSets=True",
+	"NetFlowConnectionString": "server=172.24.34.11;database=Dev_NetFlow;uid=netFlowUser;pwd=NetFlow@123456;",
+	"OrderMediatorMongoConnectionString": "mongodb://root:Passw0rd@172.24.35.21:27017/",
+	"OrderSwitchRedisConnectionString": "172.24.65.13:16379,password=aK549l327v",
+	"TseTransmitterConnectionString": "Data Source=172.30.70.11;Initial Catalog=Dev_TSETransmitter;User ID=transmitterUser;Password=Transmitter@123456;MultipleActiveResultSets=True",
+	"WalletConnectionString": "Server=172.30.70.11;Database=Dev_CashFlow;uid=cfl;pwd=Cfl@123456;",
+	"WatchlistConnectionString": "Data Source=172.24.34.11;Initial Catalog=Dev_Watchlist;User ID=WatchList_User;Password=Watchlist@1234;MultipleActiveResultSets=True;",
+            //------------------  Order Mediator Keys ------------------------------
+    "OrderMediatorSQLConnectionString":  "Data Source=172.24.34.11;Initial Catalog=Dev_OrderStore;User ID=oms;Password=Oms@123456;MultipleActiveResultSets=True;Application Name=My Application;Connection Timeout=120;TrustServerCertificate=True",
+                //------------------  Credit Management Keys ------------------------------
+    "CreditManagementSQLConnectionString":  "Data Source=172.24.34.11;Initial Catalog=DEV-Credit;User ID=credit-management;Password=123456;MultipleActiveResultSets=True;Application Name=My Application;Connection Timeout=120;TrustServerCertificate=True"
 }
-
  ;
