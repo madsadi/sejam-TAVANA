2bc1091c6ddd61bdebcb9fc17439802bbb88ecc7
madsadi <sadixus@gmail.com>
2024-01-07 16:00:54 +0330

    captcha input

--- components/common/component/captcha.tsx
+++ components/common/component/captcha.tsx
@@ -48,7 +48,7 @@ const CaptchaComponent: React.FC<any> = ({
       </label>
       <div className={"h-[54px] relative bg-inputbg flex"}>
         <input
-          className={`input pl-[182px] grow ${
+          className={`input grow rounded-l-none ${
             meta.touched && meta.error ? "border-red-300" : ""
           }`}
           type={"number"}
@@ -60,13 +60,13 @@ const CaptchaComponent: React.FC<any> = ({
         <div
           role={"button"}
           className={
-            "flex w-fit absolute left-0 before:block before:absolute before:w-0.5 before:right-0 before:bg-black/70 before:h-4/5 before:top-1/2 before:-translate-y-1/2 h-full px-4 hover:bg-border transition-all"
+            "flex w-fit relative left-0 before:block before:absolute before:w-0.5 before:right-0 before:bg-black/70 before:h-4/5 before:top-1/2 before:-translate-y-1/2 h-full px-4 hover:bg-border transition-all"
           }
           onClick={() => infoUpdate("uuid", uuidv4())}
         >
-          <div className={"overflow-hidden w-fit p-1 pl-5"}>
+          <div className={"overflow-hidden w-fit p-1 pl-3"}>
             <img
-              className={"h-full w-[100px] bg-contain"}
+              className={"h-full w-[150px] bg-contain"}
               src={generatedCaptcha}
             />
           </div>
